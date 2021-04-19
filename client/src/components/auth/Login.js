ec4d957964ef462ef7ce2a3f7b9a184c49523d32
Utkarsh Tiwari <u.tiwari@huulke.com>
2021-04-20 03:23:23 +0530

    Add auth context and implement alerts

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,63 @@
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Login = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (email === '' || password === '') {
+      setAlert('Please enter all fields.', 'danger');
+    } else {
+      console.log('Login submit!');
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <input
+          type='submit'
+          value='Login'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
